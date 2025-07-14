360e2c12713995df04616892400f3d2e20c97476
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-07-14 17:14:37 -0500

    config env in products

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { envs } from './config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -12,6 +13,8 @@ async function bootstrap() {
     }),
   )
 
-  await app.listen(process.env.PORT ?? 3000)
+  await app.listen(envs.port)
+
+  console.log(`App is running on port ${envs.port}`)
 }
 bootstrap()

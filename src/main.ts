d63fe9c0d248eecd49f3b3b9ee1989745ae5f7ce
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-07-15 16:16:38 -0500

    transform Products in Microservice

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { Logger, ValidationPipe } from '@nestjs/common'
 import { envs } from './config'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 async function bootstrap() {
   const logger = new Logger('Main')
 
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  )
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,8 +24,8 @@ async function bootstrap() {
     }),
   )
 
-  await app.listen(envs.port)
+  await app.listen()
 
-  logger.log(`App is running on port ${envs.port}`)
+  logger.log(`Products Microservice is running on port ${envs.port}`)
 }
 bootstrap()
